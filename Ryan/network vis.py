00866a4a499827d2fe98418cd4e08ea891a10f45
RYNO8 <35283610+RYNO8@users.noreply.github.com>
2020-10-01 15:22:45 +1000

    User graph vis finished
    Feeatures:
     - double click brings up overlay, with link to bleahs
     - single click "bolds" current node and 1st degree, 2nd degree neighbours
     - node image is users avatar
     - tooltip on hover
     - filter nodes based on favourite language
     - bfs to find friends
     - (debug) store graph in pickled form, to reduce loading time and number of github api requests

--- Ryan/network vis.py
+++ Ryan/network vis.py
@@ -1,26 +1,82 @@
 from flask import *
+from github import Github, NamedUser
+import pickle
+import string
 
 app = Flask(__name__)
-@app.route("/")
+g = Github("test-user1337", "cyrilhas2iq")
+SEARCH_NODES = 20
+
+def favLanguage(user):
+    if isinstance(user, NamedUser.NamedUser):
+        return user.login[0].title()
+    elif isinstance(user, str):
+        return user[0].title()
+    else:
+        raise
+        
+def bfs(user):
+    """treats all edges as bidirectional edges based on followers (not followings)"""
+    #time taken exponential to DEPTH and len(followers)
+    
+    todo = [user] # list of user objects
+    allUsers = [user] # list of of user objects
+    edges = [] # list of tuples representing edges (string, string)
+    for depth in range(SEARCH_NODES):
+        curr = todo.pop(0)
+        print(curr)
+        
+        for child in list(curr.get_followers()):
+            if not child in allUsers:
+                todo.append(child)
+                allUsers.append(child)
+            edges.append((allUsers.index(curr), allUsers.index(child)))
+            
+    edgeDirection = []
+    while edges:
+        curr = edges.pop()
+        if (curr[1], curr[0]) in edges: # edge is bidirectional
+            edges.remove((curr[1], curr[0]))
+            edgeDirection.append((*curr, "to, from"))
+        else: # edge is not bidirectional
+            edgeDirection.append((*curr, "to"))
+    
+    return allUsers, edgeDirection
+
+@app.route("/graph", methods=["GET", "POST"])
 def main():
-    graph = eval(open("graph_smol.txt", "r").readline())
-    allUsers = set()
-    edges = set()
-    for sNode, eNodes in graph.items():
-        allUsers.add(sNode)
-        for eNode in eNodes:
-            edges.add((sNode, eNode))
-            allUsers.add(eNode)
-    nameToNode = {v: k for k, v in enumerate(allUsers)}
-    #nodeToName = {k: v for k, v in enumerate(graph.keys())}
+    """\
+get request: view graph for default user ("RYNO8")
+post request {"user": x}: view graph for user x"""
+    user = "RYNO8"
+    if request.method == "GET":
+        user = request.args.get("user", user)
+    elif request.method == "POST":
+        user = request.form.get("user", user)
+    print(user)
+    
+    try:
+        allUsers, edges = pickle.load(open(f"cache\{user}", "rb"))
+    except FileNotFoundError:
+        allUsers, edges = bfs(g.get_user(user))
+        
+    with open(f"cache\{user}", "wb") as f:
+        pickle.dump((allUsers, edges), f)
     
     return render_template(
         "network vis.html",
-        nodes=", ".join([f"{{id: {v}, label: '{k}'}}" for k, v in nameToNode.items()]),
-        edges=", ".join([f"{{from: {nameToNode[k]}, to: {nameToNode[v]}}}" for k, v in edges])
+        nodeLabels=list(string.ascii_uppercase), #TODO: fix
+        nodes=", ".join([f"""{{
+            id: {i},
+            label: '{user.login}',
+            value: {sum(i in edge for edge in edges)**1.3},
+            lang: '{favLanguage(user)}',
+            shape: 'circularImage',
+            image: '{user.avatar_url}',
+            title: 'TODO: tooltip text',
+        }}""" for i, user in enumerate(allUsers)]),
+        edges=", ".join([f"{{ from: {i[0]}, to: {i[1]}, arrows: '{i[2]}' }}" for i in edges])
     )
 
 app.run(debug=True)
 
-
-# GET /submit user=...
